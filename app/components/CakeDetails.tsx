63ea4da43d780bfb76df3262a0cb438a9f58b3af
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2025-01-11 17:56:06 +0530

    update cake detail page ui

--- app/components/CakeDetails.tsx
+++ app/components/CakeDetails.tsx
@@ -2,15 +2,16 @@
 
 import { useState, useEffect, useRef } from "react";
 import ImageMagnifier from "@/components/common/ImageMagnifier";
-import { Button } from "@/components/ui/button";
 import { GrSquare } from "react-icons/gr";
 import Loader from "./Loader";
-import { FaPlayCircle } from "react-icons/fa"; // Example video icon from React Icons
+import { FaPlayCircle } from "react-icons/fa";
 import Image from "next/image";
 import { useCart } from "@/context/CartContext";
+import { Button } from "@/components/ui/button";
+//import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface Price {
-  weight: number; // Weight in grams or kilograms
+  weight: number;
   costPrice: number;
   sellPrice: number;
 }
@@ -25,17 +26,13 @@ interface Cake {
   category: string;
 }
 
-// Helper function to convert YouTube URL to embed format
 const convertToEmbedUrl = (videoUrl: string): string | null => {
   try {
     if (!videoUrl || typeof videoUrl !== "string") return null;
 
-    // Trim whitespace from the URL
     videoUrl = videoUrl.trim();
 
-    // Check if the URL starts with "http"
     if (videoUrl.toLowerCase().includes("http")) {
-      // Extract video ID for different patterns (YouTube and Vimeo)
       const videoIdMatch = videoUrl.match(
         /(?:\?v=|&v=|\/v\/|\/embed\/|youtu\.be\/|\/shorts\/|\/)([a-zA-Z0-9_-]{11})/
       );
@@ -43,7 +40,6 @@ const convertToEmbedUrl = (videoUrl: string): string | null => {
       if (videoIdMatch && videoIdMatch[1]) {
         const videoId = videoIdMatch[1];
 
-        // Handle YouTube cases
         if (
           videoUrl.toLowerCase().includes("youtube") ||
           videoUrl.toLowerCase().includes("youtu.be")
@@ -52,7 +48,6 @@ const convertToEmbedUrl = (videoUrl: string): string | null => {
         }
       }
 
-      // Handle Vimeo cases
       if (videoUrl.toLowerCase().includes("vimeo")) {
         const vimeoIdMatch = videoUrl.match(/(?:vimeo\.com\/)([0-9]+)/);
         if (vimeoIdMatch && vimeoIdMatch[1]) {
@@ -61,7 +56,6 @@ const convertToEmbedUrl = (videoUrl: string): string | null => {
         }
       }
     } else {
-      // Assume input is a direct video ID or some invalid format
       const alphaRegex = /[a-zA-Z]/;
       if (alphaRegex.test(videoUrl)) {
         return `https://www.youtube.com/embed/${videoUrl}`;
@@ -70,33 +64,30 @@ const convertToEmbedUrl = (videoUrl: string): string | null => {
       }
     }
 
-    return null; // Fallback for unrecognized URLs
+    return null;
   } catch (error) {
     console.error("Error processing video URL:", error);
-    return null; // Return null in case of an error
+    return null;
   }
 };
+
 const getThumbnailUrl = (url: string): string | null => {
   if (!url || typeof url !== "string") return null;
 
-  // Check if it's a YouTube video URL
   const isYouTubeVideo =
     url.includes("youtube.com") || url.includes("youtu.be");
 
   if (isYouTubeVideo) {
-    // Extract video ID for different YouTube URL patterns
     const videoIdMatch = url.match(
       /(?:\?v=|&v=|\/v\/|\/embed\/|youtu\.be\/|\/shorts\/|\/)([a-zA-Z0-9_-]{11})/
     );
 
     if (videoIdMatch && videoIdMatch[1]) {
       const videoId = videoIdMatch[1];
-      // Return the YouTube video thumbnail URL
       return `https://img.youtube.com/vi/${videoId}/0.jpg`;
     }
   }
 
-  // If it's not a video, assume it's an image URL
   return url;
 };
 
@@ -104,8 +95,9 @@ export default function CakeDetails({ id }: { id: string }) {
   const [cake, setCake] = useState<Cake | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [select, setSelect] = useState<number>(0);
-  const [selectedWeightIndex, setSelectedWeightIndex] = useState<number>(0);
   const thumbnailsContainerRef = useRef<HTMLDivElement>(null);
+  const [selectedWeight, setSelectedWeight] = useState<Price | null>(null);
+
   const { addToCart } = useCart();
 
   useEffect(() => {
@@ -114,6 +106,7 @@ export default function CakeDetails({ id }: { id: string }) {
         const response = await fetch(`/api/cakes/${id}`);
         const data = await response.json();
         setCake(data);
+        setSelectedWeight(data.prices[0]);
       } catch (error) {
         console.error("Error fetching cake details:", error);
       } finally {
@@ -125,12 +118,13 @@ export default function CakeDetails({ id }: { id: string }) {
   }, [id]);
 
   const handleAddToCart = (cake: Cake) => {
-    const selectedPrice = cake.prices[selectedWeightIndex];
+    if (!selectedWeight) return;
+
     addToCart({
       id: id,
       name: cake.name,
-      price: selectedPrice.sellPrice,
-      weight: selectedPrice.weight,
+      price: selectedWeight.sellPrice,
+      weight: selectedWeight.weight,
       quantity: 1,
       image: cake.image[0],
     });
@@ -152,125 +146,176 @@ export default function CakeDetails({ id }: { id: string }) {
     );
   }
 
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="flex flex-col lg:flex-row gap-4">
-        {/* First Column: Scrollable Thumbnail List */}
-        <div className="w-full lg:w-[10%] relative">
-          <div
-            ref={thumbnailsContainerRef}
-            className="h-[150px] sm:h-[150px] lg:h-[500px] overflow-x-auto lg:overflow-y-auto scrollbar-hide flex flex-row lg:flex-col items-center gap-2"
-            style={{
-              msOverflowStyle: "none",
-              scrollbarWidth: "none",
-            }}
-          >
-            {cake.image.map((i, index) => {
-              const thumbnailUrl = getThumbnailUrl(i);
-              const isYouTubeVideo =
-                i.includes("youtube.com") || i.includes("youtu.be");
-              return (
-                <div
-                  key={index}
-                  className={`relative ${
-                    select === index
-                      ? "border-2 border-primary"
-                      : "border border-qgray-border"
-                  } w-[80px] h-[80px] sm:w-[100px] sm:h-[100px] p-[5px] cursor-pointer transition-all duration-200 hover:border-primary`}
-                  onClick={() => setSelect(index)}
-                >
-                  <Image
-                    src={thumbnailUrl || "/default-image.jpg"}
-                    alt={`Cake ${index}`}
-                    width={100}
-                    height={100}
-                    className="w-full h-full object-cover"
-                  />
-                  {isYouTubeVideo && (
-                    <FaPlayCircle
-                      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white opacity-75 text-2xl sm:text-3xl"
-                      aria-label="Play Video"
-                    />
-                  )}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        {/* Second Column: Main Image or Video */}
-        <div className="w-full lg:w-[40%] p-1">
-          <div className="relative bg-white">
-            {cake.image[select].includes("youtube.com") ||
-            cake.image[select].includes("youtu.be") ? (
-              convertToEmbedUrl(cake.image[select]) ? (
-                <iframe
-                  className="w-full h-[200px] sm:h-[300px] lg:h-[500px]"
-                  src={convertToEmbedUrl(cake.image[select])!}
-                  title={cake.name}
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-              ) : (
-                <p className="text-red-500 text-center mt-4">
-                  Video cannot be played. Invalid or restricted URL.
-                </p>
-              )
-            ) : (
-              <div className="w-full h-[200px] sm:h-[300px] lg:h-[500px] flex items-center justify-center">
-                <ImageMagnifier
-                  src={cake.image[select]}
-                  width={500}
-                  height={500}
-                  magnifierHeight={150}
-                  magnifierWidth={150}
-                  zoomLevel={2}
-                  alt={cake.name}
-                  className="max-w-full max-h-full object-contain"
+  const Thumbnails = () => (
+    <div className="flex lg:flex-col flex-row overflow-x-auto lg:overflow-y-auto lg:h-[400px] gap-2  lg:w-24 w-full">
+      <div
+        ref={thumbnailsContainerRef}
+        className="h-[150px] sm:h-[150px] lg:h-[500px] overflow-x-auto lg:overflow-y-auto scrollbar-hide flex flex-row lg:flex-col items-center gap-2"
+        style={{
+          msOverflowStyle: "none",
+          scrollbarWidth: "none",
+        }}
+      >
+        {cake.image.map((i, index) => {
+          const thumbnailUrl = getThumbnailUrl(i);
+          const isYouTubeVideo =
+            i.includes("youtube.com") || i.includes("youtu.be");
+          return (
+            <div
+              key={index}
+              className={`relative ${
+                select === index
+                  ? "border-2 border-primary"
+                  : "border border-qgray-border"
+              } w-[80px] h-[80px] sm:w-[100px] sm:h-[100px] p-[5px] cursor-pointer transition-all duration-200 hover:border-primary`}
+              onClick={() => setSelect(index)}
+            >
+              <Image
+                src={thumbnailUrl || "/default-image.jpg"}
+                alt={`Cake ${index}`}
+                width={100}
+                height={100}
+                className="w-full h-full object-contain"
+              />
+              {isYouTubeVideo && (
+                <FaPlayCircle
+                  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white opacity-75 text-2xl sm:text-3xl"
+                  aria-label="Play Video"
                 />
-              </div>
-            )}
-          </div>
-        </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 
-        {/* Third Column: Description Section */}
-        <div className="w-full lg:w-[50%]">
-          <div className="p-2">
-            <h1 className="text-2xl sm:text-3xl font-bold mb-2">{cake.name}</h1>
-            <div className="flex items-center text-green-600 mb-4">
+  const MainImage = () => (
+    <div className="relative flex-1 overflow-hidden">
+      <div className="bg-white">
+        {cake.image[select].includes("youtube.com") ||
+        cake.image[select].includes("youtu.be") ? (
+          convertToEmbedUrl(cake.image[select]) ? (
+            <iframe
+              className="w-full h-[200px] sm:h-[300px] lg:h-[500px]"
+              src={convertToEmbedUrl(cake.image[select])!}
+              title={cake.name}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          ) : (
+            <p className="text-red-500 text-center mt-4">
+              Video cannot be played. Invalid or restricted URL.
+            </p>
+          )
+        ) : (
+          <ImageMagnifier
+            src={cake.image[select]}
+            width={500}
+            height={500}
+            magnifierHeight={150}
+            magnifierWidth={150}
+            zoomLevel={2}
+            alt={cake.name}
+            className="w-full h-full object-contain"
+          />
+        )}
+      </div>
+      {/* because of this we can not use magnifier and  video paly  */}
+      {/* <div className="absolute inset-0 flex items-center justify-between p-4 opacity-0 hover:opacity-100 transition-opacity">
+        <button 
+          onClick={() => setSelect(prev => Math.max(0, prev - 1))}
+          className="p-2 rounded-full bg-white/80 shadow-lg hover:bg-white"
+          disabled={select === 0}
+        >
+          <ChevronLeft className="w-6 h-6" />
+        </button>
+        <button 
+          onClick={() => setSelect(prev => Math.min(cake.image.length - 1, prev + 1))}
+          className="p-2 rounded-full bg-white/80 shadow-lg hover:bg-white"
+          disabled={select === cake.image.length - 1}
+        >
+          <ChevronRight className="w-6 h-6" />
+        </button>
+      </div> */}
+    </div>
+  );
+
+  const ProductInfo = () => {
+    if (!selectedWeight) return null;
+
+    return (
+      <div className="flex flex-col gap-4 p-4 lg:w-80">
+        <div>
+          <h1 className="text-2xl font-bold">{cake.name}</h1>
+          <div className="flex gap-2 mt-2">
+            <span className="px-2 py-1 text-sm bg-green-100 text-green-800 rounded-full">
               <GrSquare className="mr-1" />
               <span className="text-sm font-medium">
                 {cake.type.toUpperCase()}
               </span>
-            </div>
-            <p className="text-sm sm:text-base text-gray-700 mb-4">
-              {cake.description}
-            </p>
-            <div className="mb-4">
-              <label className="text-sm font-semibold text-gray-600">
-                Select Weight:
-              </label>
-              <select
-                className="w-full mt-1 p-2 border rounded-lg"
-                value={selectedWeightIndex}
-                onChange={(e) => setSelectedWeightIndex(Number(e.target.value))}
-              >
-                {cake.prices.map((price, index) => (
-                  <option key={index} value={index}>
-                    {price.weight}Kg - ₹{price.sellPrice}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <Button
-              onClick={() => handleAddToCart(cake)}
-              className="w-full mt-4 py-3 sm:py-4 text-sm sm:text-base"
-            >
-              Add to Cart
-            </Button>
+            </span>
+            <span className="px-2 py-1 text-sm bg-blue-100 text-blue-800 rounded-full">
+              {cake.category}
+            </span>
           </div>
         </div>
+
+        <p className="text-gray-600">{cake.description}</p>
+
+        <div className="space-y-4">
+          <h2 className="font-semibold">Select Weight</h2>
+          <div className="flex gap-2">
+            {cake.prices.map((price) => (
+              <button
+                key={price.weight}
+                onClick={() => setSelectedWeight(price)}
+                className={`px-4 py-2 rounded-full border-2 transition-all duration-200
+                ${
+                  selectedWeight.weight === price.weight
+                    ? "border-blue-500 bg-blue-50"
+                    : "border-gray-200 hover:border-blue-200"
+                }`}
+              >
+                {price.weight}Kg
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <div className="space-y-2">
+          <div className="flex items-baseline gap-2">
+            <span className="text-2xl font-bold">
+              ₹{selectedWeight.sellPrice}
+            </span>
+            <span className="text-lg text-gray-500 line-through">
+              ₹{selectedWeight.costPrice}
+            </span>
+            <span className="text-green-600 text-sm">
+              {Math.round(
+                (1 - selectedWeight.sellPrice / selectedWeight.costPrice) * 100
+              )}
+              % OFF
+            </span>
+          </div>
+        </div>
+        <Button
+          onClick={() => handleAddToCart(cake)}
+          className="w-full mt-4 py-3 sm:py-4 text-sm sm:text-base"
+        >
+          Add to Cart
+        </Button>
+      </div>
+    );
+  };
+
+  return (
+    <div className="max-w-6xl mx-auto p-4">
+      <div className="flex lg:flex-row flex-col gap-4">
+        <Thumbnails />
+        <MainImage />
+        <ProductInfo />
       </div>
     </div>
   );
--- components/common/ImageMagnifier.tsx
+++ components/common/ImageMagnifier.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState, MouseEvent } from 'react';
 
 interface ImageMagnifierProps {
