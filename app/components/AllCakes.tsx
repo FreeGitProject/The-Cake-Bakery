a11a4df069357337e91568cf388696485753131c
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2025-01-18 12:34:27 +0530

    Update AllCakes.tsx

--- app/components/AllCakes.tsx
+++ app/components/AllCakes.tsx
@@ -151,7 +151,7 @@ export default function AllCakes() {
       </div>
 
       {/* Cakes Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {currentCakes.map((cake) => (
           <CakeCard key={cake._id} cake={cake} />
         ))}
