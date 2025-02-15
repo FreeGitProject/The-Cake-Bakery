28a0221d434c5bce44774a256a3cc027facec1e1
SatishGuptag <141490903+SatishGuptaG@users.noreply.github.com>
2025-02-15 13:48:56 +0530

    update profile
    update profile

---
+++ components/icons.tsx
@@ -0,0 +1,8 @@
+import { AlertTriangle, CheckCircle, Info, XCircle } from "lucide-react";
+
+export const Icons = {
+  warning: AlertTriangle,
+  success: CheckCircle,
+  info: Info,
+  error: XCircle,
+};
