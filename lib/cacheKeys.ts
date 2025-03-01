ef7aa67ef36adff56a8c2ccc9b89e08161e8d12f
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2025-03-01 21:13:14 +0530

    01-Mar-2025-AG-AddCacheinHome
    01-Mar-2025-AG-AddCacheinHome

---
+++ lib/cacheKeys.ts
@@ -0,0 +1,17 @@
+export const CACHE_KEYS = {
+    HOME: 'home',
+    FOOTER: 'footer',
+    LOCATIONS: "locations",
+    NEWS: "news",
+    FAVORITES_TOP3: "favorites_top3",
+    CAKES: (caketype: string, category: string | null, page: number, limit: number, search: string, isAdmin: boolean) =>
+      `cakes_${caketype}_category${category}_page${page}_limit${limit}_search${search}_admin${isAdmin}`,
+  };
+  
+
+//   export const CACHE_KEYS = {
+//     HOME: (isAdmin: boolean) => `home_admin${isAdmin}`,
+//     CAKES: (caketype: string, category: string | null, page: number, limit: number, search: string, isAdmin: boolean) =>
+//       `cakes_${caketype}_category${category}_page${page}_limit${limit}_search${search}_admin${isAdmin}`,
+//   };
+  
